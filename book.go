5ac78d15a89b83f465b49d7d2c9833c7d87d545d
akshar <takleakshar@gmail.com>
2018-02-26 09:42:29 -0800

    new

---
+++ book.go
@@ -0,0 +1,42 @@
+package api
+import(
+	"encoding/json"
+	"net/http"
+)
+
+type Book struct{
+	Title string `json:"title"`
+	Author string `json:"author"`
+	ISBN string	`json:"isbn"`
+}
+
+func (b Book) ToJSON []byte{
+	ToJSON,err:=json.Marshal(b)
+	if err !=nil{
+		panic(err)
+	}
+	return ToJSON
+}
+
+func FromJSON(data []byte) Book{
+	book:Book{}
+	err:= json.Unmarshal(data,&book)
+	if err !=nil{
+		panic(err)
+	}
+	return book
+}
+
+var Books=[]Book{
+	Book{Title:"CloudNative Go" Author: "Akshar Takle",ISBN:"123456"},
+	Book{Title:"CloudNative Primer" Author: "Akshar Takle",ISBN:"123133456"}
+}
+
+func BooksHandleFunc( w http.ResponseWriter, r *http.Request){
+	b,err:=json.Marshal(Books)
+	if err!=nil{
+		panic(err)
+	}
+	w.Header().Add("Content-Type","application/json","charset-utf-8")
+	w.Write(b)
+}
---
+++ microservice.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"os"
+	"book.go"
+)
+
+func main(){
+	http.HandleFunc("/",index)
+	http.ListenAndServe(port(),nil)
+
+	http.HandleFunc("/api/books",BooksHandleFunc)
+}
+
+func port() string{
+port := os.Getenv("PORT")
+	if len(port)==0{
+		port="8080"
+	}
+	return ":"+port
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"Hello Cloud Native Go")
+}
